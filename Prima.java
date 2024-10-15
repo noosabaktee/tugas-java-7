820311e400e1283dbb5bf1f88bb0da8b63e0ea7f
noosabaktee <ramanusabakti03@gmail.com>
2024-10-15 20:30:34 +0700

    update

--- Prima.java
+++ Prima.java
@@ -12,7 +12,7 @@ public class Prima {
     public static void main(String[] args) {
     int i=1;
     while(i<=20){
-      if(i%2 != 0 && i%3 != 0 || i == 2 || i == 3){
+      if(i%2 != 0 && i%3 != 0 && i>1 || i == 2 || i == 3){
         System.out.println(i + " prima");
       }else{
         System.out.println(i + " bukan");
